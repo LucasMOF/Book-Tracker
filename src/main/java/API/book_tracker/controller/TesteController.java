c286e194c9e23a63c81cb0728796402ce1d92025
Lucas Moura <felipelucasmoura350@gmail.com>
2026-03-10 14:55:46 -0300

    Implementando a liberação do tokenJWT

---
+++ src/main/java/API/book_tracker/controller/TesteController.java
@@ -0,0 +1,17 @@
+package API.book_tracker.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/teste")
+public class TesteController {
+
+    @GetMapping
+    public ResponseEntity<String> ola(){
+        return ResponseEntity.ok("Olá seu token está funcionando");
+    }
+
+}
